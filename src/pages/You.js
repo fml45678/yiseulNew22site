13772eecece53ab3c6446efc658c7ad843ba08fd
Masson LeMieux <fml45678@gmail.com>
2022-09-01 01:33:47 -0400

    added the emailjs and added email notification when text area data is sent to database, data when email is sent to database still needs an email notification

--- src/pages/You.js
+++ src/pages/You.js
@@ -2,10 +2,36 @@ import React from "react";
 import "./You.css";
 import { database } from "../firebase";
 import { set, ref } from "firebase/database";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { uid } from "uid";
+import emailjs from "@emailjs/browser";
 
 function You() {
+  // this is to send emails to info@yiseullemieux.com whenever someone submits something to the database
+  const form = useRef();
+
+  const sendEmail = () => {
+    // e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_y91c4yb",
+        "template_5rmf4zq",
+        form.current,
+        "boz7ZGcVDPwzqnoM6"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  // preparing the textarea, asking for feedback to be sent to the firebase realtime database
+
   const [feedback, setFeedback] = useState("");
 
   const handleFeedbackChange = (e) => {
@@ -17,8 +43,12 @@ function You() {
     set(ref(database, `/${uuid}`), {
       feedback,
     });
+    sendEmail();
     setFeedback("");
   };
+
+  // preparing the input asking for and email to be sent to the firebase realtime database
+
   const [email, setEmail] = useState("");
 
   const handleEmailChange = (e) => {
@@ -30,6 +60,7 @@ function You() {
     set(ref(database, `/${uuid}`), {
       email,
     });
+    sendEmail();
     setEmail("");
   };
 
@@ -43,18 +74,26 @@ function You() {
           What would you like to Experience Made, squeezed, stirred, and
           packaged by Yiseul LeMieux?
         </p>
-        <textarea
-          name=""
-          onChange={handleFeedbackChange}
-          id="youFeedback"
-          value={feedback}
-          className="you__feedbackCircle__textarea"
-        ></textarea>
+        <form id="FORM" ref={form}>
+          <textarea
+            type="text"
+            name="text_box"
+            onChange={handleFeedbackChange}
+            id="youFeedback"
+            value={feedback}
+            className="you__feedbackCircle__textarea"
+          ></textarea>
+        </form>
         <div
           onClick={submitFeedback}
           className="feedbackCircle__submitButton"
+          value="send"
         ></div>
-        <span onClick={submitFeedback} className="you__feedbackCircleText">
+        <span
+          value="send"
+          onClick={submitFeedback}
+          className="you__feedbackCircleText"
+        >
           Submit
         </span>
       </div>
@@ -74,15 +113,19 @@ function You() {
           If you would like to know about the Next adventure <br /> By Yiseul
           LeMieux?
         </p>
-        <input
-          placeholder="email"
-          onChange={handleEmailChange}
-          name=""
-          value={email}
-          type="email"
-          id="youFeedback"
-          className="you__feedbackCircle__input"
-        ></input>
+        <form
+        // id="FORM" ref={form}
+        >
+          <input
+            placeholder="email"
+            onChange={handleEmailChange}
+            name="user_name"
+            value={email}
+            type="email"
+            id="youFeedback"
+            className="you__feedbackCircle__input"
+          ></input>
+        </form>
         <div
           onClick={submitEmail}
           className="feedbackCircle__submitButton"
