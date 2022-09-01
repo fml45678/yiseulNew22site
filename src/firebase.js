dcc838982ee354782249d4002a10a5d26a13de86
Masson LeMieux <fml45678@gmail.com>
2022-09-01 13:35:07 -0400

    added descriptions to video data, commented out firebase analytics for now

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -17,5 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export const database = getDatabase(app);
