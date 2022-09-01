5aa6a8590205a59497a07760eaa1998fcc6300db
Masson LeMieux <fml45678@gmail.com>
2022-08-31 23:49:56 -0400

    added npm packages uid and firebase, set up firebase database on pageYou

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd7c5WB5Z85zwR0ALA5fJQcApv9o_jVQw",
+  authDomain: "yiseullemieux.firebaseapp.com",
+  databaseURL: "https://yiseullemieux-default-rtdb.firebaseio.com",
+  projectId: "yiseullemieux",
+  storageBucket: "yiseullemieux.appspot.com",
+  messagingSenderId: "284239759923",
+  appId: "1:284239759923:web:4d60244e8ede6163633146",
+  measurementId: "G-5Y4PLDTE06",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+export const database = getDatabase(app);
--- src/pages/You.js
+++ src/pages/You.js
@@ -1,6 +1,38 @@
 import React from "react";
 import "./You.css";
+import { database } from "../firebase";
+import { set, ref } from "firebase/database";
+import { useState } from "react";
+import { uid } from "uid";
+
 function You() {
+  const [feedback, setFeedback] = useState("");
+
+  const handleFeedbackChange = (e) => {
+    setFeedback(e.target.value);
+  };
+
+  const submitFeedback = () => {
+    const uuid = uid();
+    set(ref(database, `/${uuid}`), {
+      feedback,
+    });
+    setFeedback("");
+  };
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const submitEmail = () => {
+    const uuid = uid();
+    set(ref(database, `/${uuid}`), {
+      email,
+    });
+    setEmail("");
+  };
+
   return (
     <div className="you__container">
       <div className="you__promise">
@@ -13,11 +45,18 @@ function You() {
         </p>
         <textarea
           name=""
+          onChange={handleFeedbackChange}
           id="youFeedback"
+          value={feedback}
           className="you__feedbackCircle__textarea"
         ></textarea>
-        <div className="feedbackCircle__submitButton"></div>
-        <span className="you__feedbackCircleText">Submit</span>
+        <div
+          onClick={submitFeedback}
+          className="feedbackCircle__submitButton"
+        ></div>
+        <span onClick={submitFeedback} className="you__feedbackCircleText">
+          Submit
+        </span>
       </div>
 
       <div className="you__socialLinks">
@@ -32,17 +71,25 @@ function You() {
 
       <div id="youCirlceTwo" className="you__feedbackCircle">
         <p className="you__question">
-          If you would like to know Next adventure <br /> By Yiseul LeMieux?
+          If you would like to know about the Next adventure <br /> By Yiseul
+          LeMieux?
         </p>
         <input
           placeholder="email"
+          onChange={handleEmailChange}
           name=""
+          value={email}
           type="email"
           id="youFeedback"
           className="you__feedbackCircle__input"
         ></input>
-        <div className="feedbackCircle__submitButton"></div>
-        <span className="you__feedbackCircleText">Submit</span>
+        <div
+          onClick={submitEmail}
+          className="feedbackCircle__submitButton"
+        ></div>
+        <span onClick={submitEmail} className="you__feedbackCircleText">
+          Submit
+        </span>
       </div>
     </div>
   );
