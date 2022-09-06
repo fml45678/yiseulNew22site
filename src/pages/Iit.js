fc3b5efd26507e46d4a25c8d7e2fa01db7e71ad5
Masson LeMieux <fml45678@gmail.com>
2022-09-05 23:23:44 -0400

    temporary fix for you larger screens for sizing until I figure something else out

--- src/pages/Iit.js
+++ src/pages/Iit.js
@@ -10,29 +10,32 @@ function Iit() {
   const [showComponent, setShowComponent] = useState(false);
   const [showComponent2, setShowComponent2] = useState(false);
 
-  console.log("bio is", showComponent);
-  console.log("cv is", showComponent2);
+  // console.log("bio is", showComponent);
+  // console.log("cv is", showComponent2);
 
   return (
     <div className="iIt__container">
-      <span className="pageTitle">I, It</span>
-
-      {!showComponent && !showComponent2 && (
-        <button id="revealMe" className="revealMe">
-          Reveal me - Click circles
-        </button>
-      )}
-      <div className="iIt__bioCv">
-        <Bio
-          onClick={setShowComponent}
-          showComponent={showComponent}
-          showComponent2={showComponent2}
-        />
-        <Cv
-          onClick={setShowComponent2}
-          showComponent={showComponent2}
-          showComponent1={showComponent}
-        />
+      <div className="iIt__topContent">
+        <span id="iItPageTitle" className="pageTitle">
+          I, It
+        </span>
+        {!showComponent && !showComponent2 && (
+          <button id="revealMe" className="revealMe">
+            Reveal me - Click circles
+          </button>
+        )}
+        <div className="iIt__bioCv">
+          <Bio
+            onClick={setShowComponent}
+            showComponent={showComponent}
+            showComponent2={showComponent2}
+          />
+          <Cv
+            onClick={setShowComponent2}
+            showComponent={showComponent2}
+            showComponent1={showComponent}
+          />
+        </div>
       </div>
       {!showComponent && !showComponent2 && (
         <div className="iIt__circleContainer">
