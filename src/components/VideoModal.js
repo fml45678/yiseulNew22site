330dd98fc3eee4e12ac17ab11ae43c8ca0e3f975
Masson LeMieux <fml45678@gmail.com>
2022-09-06 09:00:37 -0400

    changed video modal size for iPad class

--- src/components/VideoModal.js
+++ src/components/VideoModal.js
@@ -21,7 +21,56 @@ const VideoModal = ({ description }) => {
           }}
           className="videoDescriptionModal"
         >
-          <span className="videoModal__close">X</span>
+          {/* <span className="videoModal__close">X</span> */}
+          <svg
+            className="bio__x"
+            xmlns="http://www.w3.org/2000/svg"
+            width="45"
+            height="45"
+            viewBox="0 0 45 45"
+          >
+            <g
+              id="Group_254"
+              data-name="Group 254"
+              transform="translate(-1314 -305.724)"
+            >
+              <circle
+                id="Ellipse_618"
+                data-name="Ellipse 618"
+                cx="22.5"
+                cy="22.5"
+                r="22.5"
+                transform="translate(1314 305.724)"
+                fill="#e35d36"
+              />
+              <g
+                id="Group_252"
+                data-name="Group 252"
+                transform="translate(1322.601 314.601)"
+              >
+                <line
+                  id="Line_7"
+                  data-name="Line 7"
+                  x2="27"
+                  y2="27"
+                  transform="translate(0.398 0.124)"
+                  fill="none"
+                  stroke="#faf2f2"
+                  strokeWidth="3"
+                />
+                <line
+                  id="Line_8"
+                  data-name="Line 8"
+                  x1="27"
+                  y2="27"
+                  transform="translate(0.398 0.124)"
+                  fill="none"
+                  stroke="#faf2f2"
+                  strokeWidth="3"
+                />
+              </g>
+            </g>
+          </svg>
           <p>{description}</p>
           <p>
             Hundred percent organic Made, Squeezed, packaged By <br /> Yiseul
