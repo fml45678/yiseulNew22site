b000f86a7aae1415bf5fb76ff324f4b64e36e02f
Masson LeMieux <fml45678@gmail.com>
2022-09-11 15:55:03 -0400

    added subHeader selector (phone size class)

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -2,26 +2,69 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./SubHeader.css";
 function SubHeader() {
+  const highlightSub = (letter) => {
+    document.querySelectorAll(".subHeader__categories").forEach((e) => {
+      e.style.backgroundColor = "transparent";
+    });
+    const circle = document.getElementById(letter).style;
+    circle.backgroundColor = "#B9B2B24D";
+  };
   return (
     <div>
       <div className="subHeader__container">
         <Link to="/page/a">
-          <span className="subHeader__categories">A</span>
+          <div
+            className="subHeader__categories"
+            id="aSub"
+            onClick={() => highlightSub("aSub")}
+          >
+            A
+          </div>
         </Link>
         <Link to="/page/b">
-          <span className="subHeader__categories">B</span>
+          <div
+            className="subHeader__categories"
+            id="bSub"
+            onClick={() => highlightSub("bSub")}
+          >
+            B
+          </div>
         </Link>
         <Link to="/page/c">
-          <span className="subHeader__categories">C</span>
+          <div
+            className="subHeader__categories"
+            id="cSub"
+            onClick={() => highlightSub("cSub")}
+          >
+            C
+          </div>
         </Link>
         <Link to="/page/d">
-          <span className="subHeader__categories">D</span>
+          <div
+            className="subHeader__categories"
+            id="dSub"
+            onClick={() => highlightSub("dSub")}
+          >
+            D
+          </div>
         </Link>
         <Link to="/page/you">
-          <span className="subHeader__categories">YOU</span>
+          <div
+            className="subHeader__categories"
+            id="youSub"
+            onClick={() => highlightSub("youSub")}
+          >
+            YOU
+          </div>
         </Link>
         <Link to="/page/iit">
-          <span className="subHeader__categories">I, It</span>
+          <div
+            className="subHeader__categories"
+            id="iItSub"
+            onClick={() => highlightSub("iItSub")}
+          >
+            I, It
+          </div>
         </Link>
       </div>
       <Outlet />
